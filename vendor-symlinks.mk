7bf078fb0e11d5deae4f7d53cdf3461c0fc7d8db
Bruno Martins <bgcngm@gmail.com>
2024-06-23 21:58:49 +0100

    socrates: Convert WiFi firmware symlinks to install_symlink targets
    Change-Id: I75bb7de4593a7992f6015317cf97e088beeba0cb

--- vendor-symlinks.mk
+++
@@ -1,9 +0,0 @@
-$(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/kiwi_v2/WCNSS_qcom_cfg.ini: $(LOCAL_INSTALLED_MODULE)
-$(call symlink-file,,/vendor/etc/wifi/kiwi_v2/WCNSS_qcom_cfg.ini,$(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/kiwi_v2/WCNSS_qcom_cfg.ini)
-ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/kiwi_v2/WCNSS_qcom_cfg.ini
-$(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/kiwi_v2/wlan_mac.bin: $(LOCAL_INSTALLED_MODULE)
-$(call symlink-file,,/mnt/vendor/persist/kiwi_v2/wlan_mac.bin,$(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/kiwi_v2/wlan_mac.bin)
-ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/kiwi_v2/wlan_mac.bin
-$(TARGET_OUT_VENDOR)/firmware/wlanmdsp.otaupdate: $(LOCAL_INSTALLED_MODULE)
-$(call symlink-file,,/data/vendor/firmware/wlanmdsp.mbn,$(TARGET_OUT_VENDOR)/firmware/wlanmdsp.otaupdate)
-ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT_VENDOR)/firmware/wlanmdsp.otaupdate
