9a7136891130a370c2b7861a25e1febacf6f57a5
TheMysticle <36317711+TheMysticle@users.noreply.github.com>
2025-04-08 12:52:07 +0200

    sm8550-common: move vibrator effect stream config to select
    Lineage changed the way their vibrator is defined so we must make changes as seen here:
    
    https://review.lineageos.org/c/LineageOS/android_device_xiaomi_socrates/+/424544

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -188,10 +188,6 @@ TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
-# Vibrator
-TARGET_QTI_VIBRATOR_EFFECT_LIB := libqtivibratoreffect.xiaomi
-TARGET_QTI_VIBRATOR_USE_EFFECT_STREAM := true
-
 # VINTF
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/configs/vintf/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
