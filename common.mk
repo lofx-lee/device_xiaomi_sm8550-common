88f4735a8de623ee80b904cfcfab3996c127f4d9
TheMysticle <36317711+TheMysticle@users.noreply.github.com>
2025-05-24 20:05:34 +0200

    sm8550-common: overlay: carrierconfig update from nuwa OS2.0.200.7.VMBCNXM
    Change-Id: Idd88818d5cce5753eb3ed9cb148d214ab67cf55d

--- common.mk
+++ common.mk
@@ -268,6 +268,7 @@ PRODUCT_COPY_FILES += \
 # Overlay
 PRODUCT_PACKAGES += \
     CarrierConfigOverlayCommon \
+    CarrierConfigMiuiOverlayCommon \
     FrameworkResOverlayCommon \
     DeviceAsWebcamResCommon \
     SystemUIOverlayCommon \
